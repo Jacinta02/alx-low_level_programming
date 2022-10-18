d3655b2e9529a996346456b9334769750c65fe75
Jacinta02 <onyeodilijacinta@gmail.com>
2022-10-17 19:44:53 -0700

    task 4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
 /**
-<<<<<<< HEAD
- * main - prints alphabet in lowercase and uppercase
-=======
  * main - prints alphabets in lowercase and uppercase
->>>>>>> 7c41011cecdce6eebf56d1bc9169d06dd29fd047
  *
  * Return: Always 0
  */
 int main(void)
-<<<<<<< HEAD
-{	char ch;
-=======
 {	
 	char ch;
->>>>>>> 7c41011cecdce6eebf56d1bc9169d06dd29fd047
-	
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch < = 'z'; ch++)
 		putchar(ch);
-	for (ch = 'A'; ch <= 'z'; ch++)
+	for (ch = 'A'; ch < = 'z'; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <studio>
+
+/**
+ * main - prints the alphabetin lowercase
+ * followed by a new line, except q and e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;																	    for (la = 'a'; la <= 'z'; la++)
+	{
+		If(la != 'e' && la != 'q')
+			putchar(la);
+	}
+	putchar('\n');
+	return (0);
+}
