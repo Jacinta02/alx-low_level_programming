a1df0e564ab3425e305c8352c010de63f4b3a06a
Jacinta02 <onyeodilijacinta@gmail.com>
2022-10-17 21:28:48 -0700

    task 5

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,7 @@ int main(void)
 	char ch;
 	for (ch = 'a'; ch < = 'z'; ch++)
 		putchar(ch);
-	for (ch = 'A'; ch < = 'z'; ch++)
+	for (ch = 'A'; ch < = 'Z'; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
