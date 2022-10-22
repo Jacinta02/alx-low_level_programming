0ad5d371c80d73de66bbf48755777e39d51c2907
Jacinta02 <onyeodilijacinta@gmail.com>
2022-10-21 17:02:11 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,20 @@
+en@ubuntu:~/0x04$ cat 0-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+       	char c;
+       	c = 'A';
+       	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	 printf("%c: %d\n", c, _isupper(c));
+	 return (0);
+}
+
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Description of more function nested loops
