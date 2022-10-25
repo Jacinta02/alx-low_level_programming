551be93421fdeedc8fb96d58daf552c884a0ccf9
jacinta02 <85346925+Jacinta02@users.noreply.github.com>
2022-10-26 00:39:04 +0100

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,25 @@
 
+#include "main.h"
+
+/**
+ * print_rev - print a reverse of a string
+ * @s:String to print in reverse
+ * Return:0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	i--;
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
