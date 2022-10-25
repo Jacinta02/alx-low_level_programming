999ab6b044f77451f4996d0b2873881669ffefda
Jacinta02 <onyeodilijacinta@gmail.com>
2022-10-25 08:46:34 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+/**
+ * _strlen - returns the length of a string
+ * @s:data to be measured
+ * Return:length of @s
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+	i++;
+	}
+	return (i);
+}
