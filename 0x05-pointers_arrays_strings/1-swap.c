acf54cf3267e8afee41919a6895ffc74455221b1
Jacinta02 <onyeodilijacinta@gmail.com>
2022-10-25 08:13:34 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,15 @@
+#include "main.h"
 
+/**
+ * swap_int - swap values
+ * @a: data to be swaped
+ * @b: data to be swaped
+ * Return:0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
