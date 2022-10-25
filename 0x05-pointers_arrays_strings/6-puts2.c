90fe1ee706545bb56820a40efb2b4073aff6cddc
jacinta02 <85346925+Jacinta02@users.noreply.github.com>
2022-10-26 00:42:50 +0100

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,22 @@
+#include "main.h"
+/**
+ * puts2 -  prints every other character of a string
+ * @str:String to print
+ * Return:0
+ */
 
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
