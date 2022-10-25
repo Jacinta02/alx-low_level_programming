b49a02aacdd1e7a100d00af683810b7f8abd90e3
Jacinta02 <onyeodilijacinta@gmail.com>
2022-10-25 16:23:18 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+/**
+ * _puts - write to stdout
+ *  @str: string to print
+ *  Return:0
+ */
+
+void _puts(char *str)
+{
+int i;
+
+	i = 0;
+	while (str[i])
+	{	_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
