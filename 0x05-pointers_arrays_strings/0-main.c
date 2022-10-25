6cf04fb39bf692757e0357decd316dfdc782f75c
Jacinta02 <onyeodilijacinta@gmail.com>
2022-10-24 21:40:34 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -9,10 +9,9 @@
 int main(void)
 {
 	int n;
-       
-	n = 402;							
+n = 402;
 printf("n=%d\n", n);
-	 reset_to_98(&n);
- 	 printf("n=%d\n", n);
-	 return (0);
+reset_to_98(&n);
+printf("n=%d\n", n);
+return (0);
 }
